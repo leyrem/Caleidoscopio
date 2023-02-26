6f91a1a72f8a7710e7c3a73bdddeebce32a09821
davidquincoces17 <david.quincoces01@estudiant.upf.edu>
2023-02-26 12:43:41 +0100

    Fix animation

--- Assets/Scenes/scripts/PlayerMovement.cs
+++ Assets/Scenes/scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed = 3f;
 
     public Animator animator;
 
@@ -16,13 +17,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f).normalized;
+        transform.position = transform.position + movement * Time.deltaTime * speed;
+
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime * 3;
-        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
-        transform.position = transform.position + vertical * Time.deltaTime * 3;
         // float h = Input.GetAxisRaw("Horizontal");
         // float v = Input.GetAxisRaw("Vertical");
 
