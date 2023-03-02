d341c5090699499cd3bcb5eb5b6d0696d77be6bf
u220037 <leyre.monreal.2022@mumail.ie>
2023-03-02 19:56:32 +0100

    Cambios leyre

---
+++ Assets/Scenes/scripts/Laser.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    private LineRenderer _lineRenderer;
+    private List<Vector3> pos_laser = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        //_lineRenderer.SetPosition(0, transform.position);
+
+        pos_laser.Add(new Vector3((float)-0.8, (float)-2.5)); // init pos
+        pos_laser.Add(new Vector3((float)5.8, (float)-2.5)); // end pos
+
+        _lineRenderer.SetPositions(pos_laser.ToArray());
+        _lineRenderer.useWorldSpace = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         _lineRenderer.SetPositions(pos_laser.ToArray());
+        RaycastHit2D hit = Physics2D.Raycast(pos_laser[0], new Vector2(1,0));
+        if (hit.collider & !hit.collider.name.Equals("Player"))
+        {
+        
+            List<Vector3> new_pos = new List<Vector3>();  
+            new_pos.Add(pos_laser[0]);
+            new_pos.Add(new Vector3(hit.point.x, hit.point.y));
+            
+            _lineRenderer.SetPositions(new_pos.ToArray());
+            
+           
+        } else if(hit.collider & hit.collider.name.Equals("Player"))
+        {
+           
+        }
+        else
+        {
+
+            _lineRenderer.SetPositions(pos_laser.ToArray());
+
+           
+        }
+
+        
+    }
+}
